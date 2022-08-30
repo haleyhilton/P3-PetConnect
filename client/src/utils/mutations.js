ba13b1723def2fb9332e50b195975c4097fd65d0
NathanStein1 <nathanstein2@gmail.com>
2022-08-29 21:09:55 -0700

    login auth

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,30 @@
 import { gql } from '@apollo/client';
 
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
 export const CREATE_USER_PHOTO = gql`
 mutation AddUserPhoto($username: String!, $media: String!) {
     addProfilePicture(username: $username, media: $media) {
