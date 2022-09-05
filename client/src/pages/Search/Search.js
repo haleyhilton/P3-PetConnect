fd33407248ffe164dcb2147db683d874d8629b60
Kadika <dryan10101@gmail.com>
2022-09-05 15:26:38 -0700

    redid Search query selectors as react refs

--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './style.css'
 
 import { useQuery, useLazyQuery } from '@apollo/client';
@@ -9,6 +9,13 @@ import { QUERY_PET_SEARCH } from '../../utils/queries';
 export default function Search() {
     const [pets, setPets] = useState([]);
     const [firstRender, setFirstRender] = useState(true);
+
+    const ageRef = useRef();
+    const breedRef = useRef();
+    const sizeRef = useRef();
+    const sexRef = useRef();
+    const colorRef = useRef();
+    const forSaleRef = useRef();
 
     const { loading, error } = useQuery(QUERY_PET_SEARCH, {
         variables: {search: null, age: null, breed: null, size: null, sex: null, color: null, for_sale: null},
@@ -23,16 +30,15 @@ export default function Search() {
     if (error) console.log(JSON.parse(JSON.stringify(error)));
 
 
-    //TODO: convert query selectors to react refs!!!!
     const [newSearch] = useLazyQuery(QUERY_PET_SEARCH, {
         variables: {
             search: null,
-            age: (document.querySelector('#age-filter').value === "all" ? null : document.querySelector('#age-filter').value),
-            breed: (document.querySelector('#breed-filter').value === "all" ? null : document.querySelector('#breed-filter').value),
-            size: (document.querySelector('#size-filter').value === "all" ? null : document.querySelector('#size-filter').value),
-            sex: (document.querySelector('#sex-filter').value === "all" ? null : document.querySelector('#sex-filter').value),
-            color: (document.querySelector('#color-filter').value === "all" ? null : document.querySelector('#color-filter').value),
-            for_sale: (document.querySelector('#forsale-filter').value === "all" ? null : document.querySelector('#forsale-filter').value)
+            age: (ageRef.current.value === "all" ? null : ageRef.current.value),
+            breed: (breedRef.current.value === "all" ? null : breedRef.current.value),
+            size: (sizeRef.current.value === "all" ? null : sizeRef.current.value),
+            sex: (sexRef.current.value === "all" ? null : sexRef.current.value),
+            color: (colorRef.current.value === "all" ? null : colorRef.current.value),
+            for_sale: (forSaleRef.current.value === "all" ? null : forSaleRef.current.value)
         },
         onCompleted: newData => {
             setPets(newData.petSearch);
@@ -53,38 +59,38 @@ export default function Search() {
         <div>
             <form className="flexy" onSubmit={handleFormSubmit}>
                 <label htmlFor="age-filter">Age: </label>
-                <select id="age-filter" name="age">
+                <select id="age-filter" name="age" ref={ageRef}>
                     <option value="all">All</option>
                     <option value="3">3</option>
                 </select>
 
                 <label htmlFor="breed-filter">Breed: </label>
-                <select id="breed-filter" name="breed">
+                <select id="breed-filter" name="breed" ref={breedRef}>
                     <option value="all">All</option>
                     <option value="Chihuahua">Chihuahua</option>
                 </select>
 
                 <label htmlFor="size-filter">Size: </label>
-                <select id="size-filter" name="size">
+                <select id="size-filter" name="size" ref={sizeRef}>
                     <option value="all">All</option>
                     <option value="small">Small</option>
                 </select>
 
                 <label htmlFor="sex-filter">Sex: </label>
-                <select id="sex-filter" name="sex">
+                <select id="sex-filter" name="sex" ref={sexRef}>
                     <option value="all">All</option>
                     <option value="male">Male</option>
                     <option value="female">Female</option>
                 </select>
                 
                 <label htmlFor="color-filter">Color: </label>
-                <select id="color-filter" name="color">
+                <select id="color-filter" name="color" ref={colorRef}>
                     <option value="all">All</option>
                     <option value="white">White</option>
                 </select>
 
                 <label htmlFor="forsale-filter">For Sale?: </label>
-                <select id="forsale-filter" name="color">
+                <select id="forsale-filter" name="forsale" ref={forSaleRef}>
                     <option value="all">All</option>
                     <option value="true">For sale</option>
                     <option value="false">Not for sale</option>
