55f5f7599b0ac07d8a10f03a5906a99bf0cf1578
NathanStein1 <nathanstein2@gmail.com>
2022-09-06 08:12:04 -0700

    PROFILE

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -6,6 +6,11 @@ import Griditem from '../Griditem/Griditem'
 
 export default function Profile(props) {
 
+
+
+
+
+// This is for the individual dog modal
     const canine = {
         name: 'Ronalds',
         description: 'An old macdonald boy',
@@ -17,6 +22,64 @@ export default function Profile(props) {
         // toggle vis
         setIsOpen(current => !current)
     };
+
+
+
+
+
+
+// This is for the posts
+
+    // OPEN AND CLOSE
+    const [isPostOpen, setIsPostOpen] = useState(true);
+
+
+    const handlePostOpen = event => {
+        // toggle vis
+        setIsPostOpen(current => !current)
+    };
+
+
+    // SET VALUE
+    const [name, setName] = useState('')
+
+    const [age, setAge] = useState('')
+
+    const [breed, setBreed] = useState('')
+
+    const [description, setDescription] = useState('')
+
+
+
+
+
+    // Submit Post Request
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const gridpost = { name, age, breed, description }
+
+        // const requestOptions ={
+        //   method: 'POST',
+        //   headers: { 'Content-Type': 'application/json'},
+        //   body: JSON.stringify({ title: 'Dog Post'})
+        // }
+        // // write fetch request
+        // fetch("filepath", requestOptions)
+        // // apppend to bottom, 
+        //     `<div class="grid-item" onClick={handleOpen} >${props.name}</div>`
+
+        console.log(gridpost)
+    }
+
+
+
+
+
+
+
+
+
 
     return (
         <div>
@@ -39,7 +102,7 @@ export default function Profile(props) {
 
 
                 <div class="posts">
-                    <button>Post</button>
+                    <button onClick={handlePostOpen}>Post</button>
 
                     Append children here. Visibile if id=id
                 </div>
@@ -55,7 +118,53 @@ export default function Profile(props) {
 
 
 
-<Griditem/>
+                <div id="myModal" class="modal" style={{ display: isPostOpen ? 'none' : 'block' }}>
+
+
+                    <div class="modal-content">
+                        <span class="close" onClick={handlePostOpen}>&times;</span>
+                        <form onSubmit={handleSubmit} class="modal-inner-wrapper">
+
+
+                            <div class="modal-inner-image" >ADD IMAGE HERE</div>
+                            <label>Name</label>
+                            <input required
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}>
+                            </input>
+                            <label>Age</label>
+                            <input required
+                                value={age}
+                                onChange={(e) => setAge(e.target.value)}></input>
+
+                            <label>Breed</label>
+                            <input required
+                                value={breed}
+                                onChange={(e) => setBreed(e.target.value)}></input>
+
+                            <label>Description</label>
+                            <textarea
+                                required
+                                value={description}
+                                onChange={(e) => setDescription(e.target.value)}></textarea>
+
+
+                            <button type='submit'>Submit</button>
+
+
+                            {name}
+                            <br></br>
+                            {age}
+                            <br></br>
+                            {breed}
+                            <br></br>
+                            {description}
+
+                        </form>
+
+                    </div>
+
+                </div>
 
 
 
