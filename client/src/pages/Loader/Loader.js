43c3e7f6b2c4be07ea6c5e452e21e0e97600cbf5
Kadika <dryan10101@gmail.com>
2022-09-09 17:58:38 -0700

    made background full yellow instead of gradient and fixed indentations

--- client/src/pages/Loader/Loader.js
+++ client/src/pages/Loader/Loader.js
@@ -3,17 +3,20 @@ import './style.css'
 
 export default function Loader() {
     return (
-        <div><div className="loader">
-            <div className="dog">
-                <div className="dog-body">
-                    <div className="dog-tail">
+        <div>
+            <div className="loader">
+                <div className="dog">
+                    <div className="dog-body">
                         <div className="dog-tail">
                             <div className="dog-tail">
                                 <div className="dog-tail">
                                     <div className="dog-tail">
                                         <div className="dog-tail">
                                             <div className="dog-tail">
-                                                <div className="dog-tail"></div>
+                                                <div className="dog-tail">
+                                                    <div className="dog-tail">
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
@@ -21,22 +24,28 @@ export default function Loader() {
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="dog-torso"></div>
-                <div className="dog-head">
-                    <div className="dog-ears">
-                        <div className="dog-ear"></div>
-                        <div className="dog-ear"></div>
+                    <div className="dog-torso">
                     </div>
-                    <div className="dog-eyes">
-                        <div className="dog-eye"></div>
-                        <div className="dog-eye"></div>
-                    </div>
-                    <div className="dog-muzzle">
-                        <div className="dog-tongue"></div>
+                    <div className="dog-head">
+                        <div className="dog-ears">
+                            <div className="dog-ear">
+                            </div>
+                            <div className="dog-ear">
+                            </div>
+                        </div>
+                        <div className="dog-eyes">
+                            <div className="dog-eye">
+                            </div>
+                            <div className="dog-eye">
+                            </div>
+                        </div>
+                        <div className="dog-muzzle">
+                            <div className="dog-tongue">
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div></div>
+        </div>
     )
 }
