bcf964ae8721bef326605fa3afd2425eda79b90d
josh jankowski <joshjankowski@gmail.com>
2022-09-21 18:02:43 -0700

    all done

---
+++ client/src/components/FileUploader/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class FilesUploader extends Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <form>
+                        <h3>React File Upload</h3>
+                        <div className="form-group">
+                            <input type="file" />
+                        </div>
+                        <div className="form-group">
+                            <button className="btn btn-primary" type="submit">Upload</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
