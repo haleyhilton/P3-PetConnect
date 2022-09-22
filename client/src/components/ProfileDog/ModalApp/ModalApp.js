1a19ff83a1612ec4b899fe9d9b5faab221b5497c
NathanStein1 <nathanstein2@gmail.com>
2022-09-21 18:01:33 -0700

    additions

--- client/src/components/ProfileDog/ModalApp/ModalApp.js
+++ client/src/components/ProfileDog/ModalApp/ModalApp.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
+import Attachments from '../ModalConditionals/Attachments';
+import Info from '../ModalConditionals/Info';
 
 export default function index() {
 
+    // const selection = useRef(<Info />)
+
+    // let selectButton = document.querySelector("#option-select")
+
+    // let content;
+
+    // useEffect(() => {
+
+    //     if (selectButton.value === "Information") {
+
+    //         selection = <Info />
+
+    //     }
+    //     else if (selectButton.value === "Attachments") {
+
+    //         content = <Attachments />
+
+    //     }
+
+    // })
+
+    // if (selection.textContent === "Information"){
+
+    //     content = <Info/>
+
+    // }
+    // else if (selection.textContent === "Attachments"){
+
+    //        content=  <Attachments/>
+
+    // }
 
 
     return (
@@ -11,6 +44,8 @@ export default function index() {
                 <option>Information</option>
                 <option>Attachments</option>
             </select>
+
+            {/* <div> {selection.current}</div> */}
 
         </div>
     )
